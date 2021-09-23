44a6d46e8804d7be584342fdbdbe6d1e287ad0a6
b3rking <ishimweeschylebersy@gmail.com>
2021-09-23 11:39:33 +0200

    done on constructor

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-class Date
+class B3rking_Time
 {
   constructor() {
     let timestamps = new Date()
-    this._day = timestamps.get
+    this._day = timestamps.getDate()
+    this._month = timestamps.getMonth()
+    this._year = timestamps.getFullYear()
+    this._hour = timestamps.getHours()
+    this._minutes = timestamps.getMinutes()
+    this._seconds = timestamps.getSeconds()
+  }
+
+  get date_jour() {
+    return this._day
+  }
+
+  get jour() {
+    return this.
   }
 }
+
+const d = new B3rking_Date()
+d.jour()
